5dbfa74fefe409fc5f6139faf6eeb4b23aba5563
Hieu Le <Le.Hieu@steadfasttech.com.au>
2020-08-20 01:05:23 +0700

    test controller

---
+++ DanceClass/App/Schedule/schedule-new.js
@@ -0,0 +1,18 @@
+﻿function BaseController() {
+    this.currentUser = null;
+    this.initialize = function (callback) {
+        userService.getCurrentUser().then(function (user) {
+            this.currentUser = user;
+            callback();
+        }.bind(this));
+    };
+}
+
+function ScheduleController() {
+    BaseController.call(this);
+    this.initialize(function () {
+        console.log(this.currentUser);
+    }.bind(this));
+}
+
+new ScheduleController();
