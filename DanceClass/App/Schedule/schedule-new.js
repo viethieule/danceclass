73e2611bcc4e2b425d5be42d6acc6627ae984a13
Hieu Le <Le.Hieu@steadfasttech.com.au>
2020-08-21 00:52:21 +0700

    move schedule methods to ctrl

--- DanceClass/App/Schedule/schedule-new.js
+++ DanceClass/App/Schedule/schedule-new.js
@@ -10,10 +10,41 @@
 }
 
 function ScheduleController() {
+    var _self = this;
     BaseController.call(this);
 
+    const TIME_SLOTS = [
+        { hours: 9, minutes: 0 },
+        { hours: 10, minutes: 0 },
+        { hours: 11, minutes: 0 },
+        { hours: 12, minutes: 0 },
+        { hours: 17, minutes: 0 },
+        { hours: 18, minutes: 0 },
+        { hours: 18, minutes: 30 },
+        { hours: 19, minutes: 0 },
+        { hours: 19, minutes: 35 },
+        { hours: 20, minutes: 0 },
+    ];
+
+    this.currentDaysOfWeek = [];
+    this.scheduleDetails = [];
+
     this.run = function () {
-        console.log(this.currentUser);
+        collapseSideBar();
+        renderUserRemainingSessions();
+        initCreateScheduleForm();
+    }
+
+    function renderUserRemainingSessions() {
+        let user = _self.currentUser;
+        if (user && user.activePackage) {
+            let remainingSessions = user.activePackage.remainingSessions;
+            $('.calendar-remaining-sessions').html(formatRemainingSessions(remainingSessions));
+        } else if (user && user.roleNames.includes("Member")) {
+            $('.calendar-remaining-sessions').html(0);
+        } else {
+            $('.calendar-remaining-sessions-area').hide();
+        }
     }
 }
 
