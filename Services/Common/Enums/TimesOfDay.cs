c496688a288dc2dfca4e278f1017c7db75e313ae
viethieule <le.hieu@steadfasttech.com.au>
2021-01-22 16:52:52 +0700

    weekly schedule group by branch

---
+++ Services/Common/Enums/TimesOfDay.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services.Common.Enums
+{
+    public enum TimesOfDay
+    {
+        Morning = 0,
+        Afternoon = 12,
+        Evening = 17
+    }
+}
--- Services/Schedule/ScheduleDetailDTO.cs
+++ Services/Schedule/ScheduleDetailDTO.cs
@@ -1,4 +1,5 @@
-﻿using Services.Registration;
+﻿using Services.Common.Enums;
+using Services.Registration;
 using System;
 using System.Collections.Generic;
 
@@ -18,5 +19,23 @@ namespace Services.Schedule
         public int SessionNo { get; set; }
         public bool IsCurrentUserRegistered { get; set; }
         public RegistrationDTO CurrentUserRegistration { get; set; }
+        public TimesOfDay TimeOfDay
+        {
+            get
+            {
+                int hourStart = this.Schedule.StartTime.Hours;
+                if (hourStart >= (int)TimesOfDay.Morning && hourStart < (int)TimesOfDay.Afternoon)
+                {
+                    return TimesOfDay.Morning;
+                }
+                else if (hourStart >= (int)TimesOfDay.Afternoon && hourStart < (int)TimesOfDay.Evening)
+                {
+                    return TimesOfDay.Afternoon;
+                }
+                else
+                {
+                    return TimesOfDay.Evening;
+                }
+            }
+        }
     }
-}
